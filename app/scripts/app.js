51a17af2c60af90972e655d10764f980f09bd5cd
Jed Mao <jed.hunsakergmail>
2014-01-17 17:25:40 -0600

    Introduce HTML5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,23 @@
 'use strict';
 
 angular.module('volusionApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+
+      $locationProvider.html5Mode(true);
+
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }
+  ]);
