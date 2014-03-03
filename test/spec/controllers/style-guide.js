82a5ec7f85caf0a9a7ef7b0f6100b6e4e1c979f5
Jed Mao <jed.hunsakergmail>
2014-03-03 11:48:13 -0600

    Introduce mocha sinon-chai chai-jquery

--- test/spec/controllers/style-guide.js
+++ test/spec/controllers/style-guide.js
@@ -1,22 +1,18 @@
 'use strict';
 
+// ReSharper disable WrongExpressionStatement
 describe('Controller: StyleGuideCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('volusionApp'));
 
-  // ReSharper disable once InconsistentNaming
   var controller;
   var scope;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     controller = $controller('StyleGuideCtrl', {
       $scope: scope
     });
   }));
-
-  it('', function() {});
 
 });
