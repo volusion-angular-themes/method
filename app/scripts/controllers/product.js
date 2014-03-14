bb299ee08317432d8370052e55648b4d2dc2f444
Ken Jackson <ken@kenjacksondesign.com>
2014-03-14 17:55:45 -0500

    Added apiary calls for product, index, and home.   Added translation text as well. index changes: translation, menu fed from apiary

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -3,7 +3,17 @@
 module.exports = [
   '$scope',
   'product',
-  function($scope, product) {
+  'api',
+  '$stateParams',
+  function ($scope, product, api, $stateParams) {
     $scope.product = product;
+
+    api.relatedproducts.query({ productCode: 123, filter: 'relatedProducts', pageNumber: 1, pageSize: 10 }).then(function(data) {
+      $scope.relatedProducts = data;
+    });
+
+    api.reviews.get({ code: $stateParams.productCode }).then(function (data) {
+      $scope.ratingsAndReviews = data;
+    });
   }
 ];
