3e924ea3f5aa4683ca41060aee9bfc2c7d2a1f71
Ken Jackson <ken@kenjacksondesign.com>
2014-03-21 15:51:39 -0500

    Added product page with product endpoint exposed

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -2,12 +2,8 @@
 
 module.exports = [
   '$scope',
-  function($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.htmlReady();
+  'category',
+  function ($scope, category) {
+    $scope.category = category;
   }
 ];
--- test/spec/controllers/category.js
+++ test/spec/controllers/category.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // ReSharper disable WrongExpressionStatement
-describe('Controller: CategoryCtrl', function() {
+describe('Controller: CategoryCtrl', function () {
 
   // load the controller's module
   beforeEach(module('volusionApp'));
@@ -9,17 +9,24 @@ describe('Controller: CategoryCtrl', function() {
   // ReSharper disable once InconsistentNaming
   var controller;
   var scope;
+  var category = {
+    id: 1,
+    name: 'Category 1',
+    subCategories: [],
+    products: []
+  };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     controller = $controller('CategoryCtrl', {
-      $scope: scope
+      $scope: scope,
+      category: category
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings).to.have.length(3);
+  it('should attach the product data to the scope', function () {
+    expect(scope.category).to.deep.equal(category);
   });
 
 });
