af0d038ef7d6caa40fdbfe6e695fdb353fc434ec
Ken Jackson <ken@kenjacksondesign.com>
2014-04-30 20:41:40 -0500

    added new slides to home page

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -13,9 +13,10 @@ module.exports = [
     // TODO: REPLACE SLIDER WITH COMPONENT DATA
     $scope.interval = 4000;
     $scope.slider = [
-      { imageUrl: 'http://www.volusioninnovationlab.com/snippets/images/slide1.jpg', headline: 'New Year Saleathon', subHeadline: 'Save 20% off storewide with coupon code NEWYEAR', linksTo: 'product1/p/123', htmlContent: '<a href=\"http:\/\/www.google.com\/\">Click here<\/a>' },
-      { imageUrl: 'http://www.volusioninnovationlab.com/snippets/images/slide2.jpg', headline: 'New Sunrise Collection', subHeadline: 'Wake up to something good', linksTo: 'category1/c/123' },
-      { imageUrl: 'http://www.volusioninnovationlab.com/snippets/images/slide3.jpg', headline: 'Miami Fashion', subHeadline: null, linksTo: 'product2/p/2' }
+      { imageUrl: 'http://design16.volusion.com/v/theme-engine/method/slide1.jpg', headline: 'New Year Saleathon', subHeadline: 'Save 20% off storewide with coupon code NEWYEAR', linksTo: 'product1/p/123', htmlContent: '<a href=\"http:\/\/www.google.com\/\">Click here<\/a>' },
+      { imageUrl: 'http://design16.volusion.com/v/theme-engine/method/slide2.jpg', headline: 'New Sunrise Collection', subHeadline: 'Wake up to something good', linksTo: 'category1/c/123' },
+      { imageUrl: 'http://design16.volusion.com/v/theme-engine/method/slide3.jpg', headline: 'Miami Fashion', subHeadline: null, linksTo: 'product2/p/2' },
+      { imageUrl: 'http://design16.volusion.com/v/theme-engine/method/slide4.jpg', headline: 'Miami Fashion', subHeadline: null, linksTo: 'product2/p/2' }
     ];
 
     // TODO: REPLACE FEATURED HOME ITEMS WITH THEME PAGE SETTINGS
