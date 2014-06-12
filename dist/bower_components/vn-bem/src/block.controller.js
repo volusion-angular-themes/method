d09780c0d8b22ba99d1db988131b7c0a36c87b8e
Kevin Sproles <aspire2x@gmail.com>
2014-06-11 19:37:49 -0500

    Release Changes: * added dist folder

---
+++ dist/bower_components/vn-bem/src/block.controller.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+module.exports = [
+  function() {
+
+    this.getBlock = function() {
+      return this.block;
+    };
+
+    this.getModifiers = function() {
+      return this.modifiers;
+    };
+
+  }
+];
