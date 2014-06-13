5c570502475f9c6f7d68b5c47930d3ce6ce39da7
Jed Mao <jed.hunsakergmail>
2014-06-13 13:59:04 -0500

    bower install vn-product-option

---
+++ app/bower_components/vn-labeled-radio/src/labeled-radio.directive.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+module.exports = [
+  function() {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      replace: true,
+      transclude: true,
+      templateUrl: 'labeled-radio.html',
+      scope: {
+        ngModel: '=',
+        name: '@',
+        value: '@',
+        ngValue: '=',
+        ngChecked: '=',
+        ngDisabled: '@',
+        change: '&ngChange'
+      },
+      compile: function(tElement, tAttrs) {
+        var $radio = tElement.find('input');
+
+        var value = tAttrs.value;
+        var ngValue = tAttrs.ngValue;
+        if (typeof value !== 'undefined' && typeof ngValue === 'undefined') {
+          $radio.removeAttr('data-ng-value');
+        }
+      }
+    };
+  }
+];
---
+++ app/bower_components/vn-product-option/src/bower_components/vn-labeled-radio/src/bower_components/vn-bem/src/block.directive.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+module.exports = [
+  'bem',
+  function(bem) {
+    return {
+      restrict: 'A',
+      controller: 'BlockCtrl',
+      compile: function() {
+        return {
+          pre: function(scope, iElement, iAttrs, controller) {
+            var block = iAttrs.vnBlock;
+            var modifiers = iAttrs.vnModifiers;
+            bem.addClasses(iElement, {
+              block: block,
+              blockModifiers: modifiers
+            });
+            controller.block = block;
+            controller.modifiers = modifiers;
+          }
+        };
+      }
+    };
+  }
+];
