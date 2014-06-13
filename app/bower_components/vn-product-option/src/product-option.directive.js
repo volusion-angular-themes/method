1ae1257b20808cb4d34ba9525cf48fad3b24cb06
Jed Mao <jed.hunsakergmail>
2014-06-13 13:59:04 -0500

    bower install --save vn-product-option

---
+++ app/bower_components/vn-product-option/src/product-option.directive.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+module.exports = [
+  function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      controller: 'ProductOptionCtrl',
+      templateUrl: 'product-option.html',
+      scope: {
+        option: '=',
+        product: '=',
+        cartItem: '=',
+        change: '&ngChange'
+      }
+    };
+  }
+];
