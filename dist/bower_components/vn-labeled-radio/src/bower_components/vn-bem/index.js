27e003b46966a107a071c359ab99238a9ffcfc22
Jed Mao <jed.hunsakergmail>
2014-06-19 11:22:14 -0500

    build dist

---
+++ dist/bower_components/vn-labeled-radio/src/bower_components/vn-bem/index.js
@@ -0,0 +1,10 @@
+﻿/* global angular: true */
+'use strict';
+
+var angular = require('angular');
+
+module.exports = angular.module('vn-bem', [])
+  .directive('vnBlock', require('./src/block.directive'))
+  .directive('vnElement', require('./src/element.directive'))
+  .controller('BlockCtrl', require('./src/block.controller'))
+  .factory('bem', require('./src/bem.service'));
