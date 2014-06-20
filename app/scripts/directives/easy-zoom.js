8d7bfe17371feb2c15ac93f6e242ace8d1c27319
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-06-20 14:05:47 -0700

    Added EASY-ZOOM directive

---
+++ app/scripts/directives/easy-zoom.js
@@ -0,0 +1,89 @@
+//'use strict';
+//
+///**
+// * @ngdoc directive
+// * @name volusionMethodThemeApp.directive:easyZoom
+// * @description
+// * # easyZoom
+// */
+//angular.module('volusionMethodThemeApp')
+//  .directive('easyZoom', function () {
+//    return {
+//      template: '<div></div>',
+//      restrict: 'E',
+//      link: function postLink(scope, element, attrs) {
+//        element.text('this is the easyZoom directive');
+//      }
+//    };
+//  });
+
+/*global angular */
+
+// http://i-like-robots.github.io/EasyZoom/
+angular.module('Volusion.directives')
+    .directive('easyZoom', function () {
+
+        'use strict';
+
+        var imageHash = {};
+
+        function swapImages(zoomApi) {
+            if (imageHash.standardSrc && imageHash.zoomSrc) {
+                zoomApi.swap(imageHash.standardSrc, imageHash.zoomSrc);
+                imageHash = {};
+            }
+        }
+
+        return {
+            restrict   : 'A',
+            replace    : true,
+            templateUrl: 'template/easyZoom.html',
+            scope      : {
+                ngSrc     : '=',
+                ezAdjacent: '=',
+                ezOverlay : '=',
+                ezZoomSrc : '=',
+                alt       : '@'
+            },
+            link       : function (scope, element) {
+                var easyzoom = element.easyZoom(),
+                    api = easyzoom.data('easyZoom');
+
+                scope.$watch('ngSrc', function (newValue) {
+                    if (newValue === undefined) {
+                        return;
+                    }
+
+                    imageHash.standardSrc = newValue;
+                    swapImages(api);
+                });
+
+                scope.$watch('ezZoomSrc', function (newValue) {
+                    if (newValue === undefined) {
+                        return;
+                    }
+
+                    imageHash.zoomSrc = newValue;
+                    swapImages(api);
+                });
+
+                scope.$on('$destroy', function () {
+                    api.teardown();
+                });
+            }
+        };
+    })
+    .run(['$templateCache', function ($templateCache) {
+
+        'use strict';
+
+        $templateCache.put(
+            'template/easyZoom.html',
+            '<div class="easyzoom" data-ng-class="{ \'easyzoom--adjacent\': ezAdjacent, \'easyzoom--overlay\': ezOverlay }">' +
+                '<a data-ng-href="{{ezZoomSrc}}">' +
+                    '<img class="img-responsive" data-ng-src="{{ngSrc}}" alt="{{alt}}">' +
+                    '<div class="th-product-view__zoom"></div>' +
+                '</a>' +
+            '</div>'
+        );
+    }]);
