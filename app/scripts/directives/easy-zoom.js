fa6776e3ea4ee3ed4d13b1d0bd962faaff37976b
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-06-20 16:20:57 -0700

    Put back zoom image

--- app/scripts/directives/easy-zoom.js
+++ app/scripts/directives/easy-zoom.js
@@ -82,7 +82,7 @@ angular.module('Volusion.directives')
             '<div class="easyzoom easyzoom--adjacent" data-ng-class="{ \'easyzoom--adjacent\': ezAdjacent, \'easyzoom--overlay\': ezOverlay }">' +
                 '<a data-ng-href="{{ezZoomSrc}}">' +
                     '<img class="img-responsive" data-ng-src="{{ngSrc}}" alt="{{alt}}">' +
-                    //'<div class="th-product-view__zoom"></div>' +
+                    '<div class="th-product-view__zoom"></div>' +
                 '</a>' +
             '</div>'
         );
