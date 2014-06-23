febe0bff52892a213653fd1e3d312a92fa626d8c
Jason Covington <jasoncovington@me.com>
2014-06-23 14:20:28 -0500

    index js

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -115,3 +115,14 @@ angular.module('volusion.controllers').controller('IndexCtrl', [
     });
   }
 ]);
+
+$(document).ready(function() {
+  $('[data-toggle="popover"]').popover();
+  $('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+      if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+        $(this).popover('hide');
+      }
+    });
+  });
+});
