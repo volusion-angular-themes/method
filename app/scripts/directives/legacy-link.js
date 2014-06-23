63fd910373d27b22d9b362014ebf97341e0dcfb8
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-06-23 16:06:34 -0700

    Ported LEGACY LINK.

---
+++ app/scripts/directives/legacy-link.js
@@ -0,0 +1,26 @@
+/*global angular */
+
+/**
+ * @ngdoc directive
+ * @name volusionMethodThemeApp.directive:legacyLink
+ * @description
+ * # legacyLink
+ */
+angular.module('Volusion.directives')
+    .directive('legacyLink',
+        ['$window',
+            function ($window) {
+
+                'use strict';
+
+                return {
+                    restrict: 'A',
+                    link: function (scope, element, attrs) {
+                        element.attr('href', attrs.legacyLink);
+                        element.on('click', function (e) {
+                            e.preventDefault();
+                            $window.location.assign(this.href);
+                        });
+                    }
+                };
+            }]);
