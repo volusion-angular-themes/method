c7d6e7ff2c9a7c355f427ce79736e91ef9a37fc3
Matt Hippely <hippely@gmail.com>
2014-06-23 17:22:03 -0700

    Refactor Method: * Faceted Search Progress via category page.

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -8,125 +8,31 @@ angular.module('methodApp')
             $rootScope.seo = {};
 
             /**
-             * Private functions for the CategoryCtrl
-             */
-
-            /**
-             * @ngdoc method
-             * @name getFacetsFromResponse
-             * @methodOf methodApp.CategoryCtrl
-             * @params {Object} response
-             * @returns {Object} facets is an object parsed from response.facets
-             *
-             * @description
-             * The vn-faceted-search directive needs the facets data abject response from product queries on first page load.
-             * This can boot strap the refinement process with the facets for this venders products.
-             *
-             */
-            function getFacetsFromResponse(response) {
-
-                if(response.hasOwnProperty(facets)) {
-                    return response.facets;
-                }
-            }
-
-            /**
-             * @ngdoc method
-             * @name getBrandFromFacets
-             * @methodOf methodApp.CategoryCtrl
-             * @params {Object} facet
-             * @returns {Object} brand is an object parsed from response.facets when calling the product endpoint
-             *
-             * @description
-             * In order to make use of brand specific data in the category view the CategoryCtrl needs to parse it and
-             * set $scope variables that are accessible in the category.html view. This is a private
-             *
-             */
-            function getBrandFromFacets(facets) {
-                var item,
-                    obj;
-
-                if (facets.hasOwnProperty(item)) {
-                    for (item in facets) {
-                        obj = facets[item];
-                        if (obj.title && obj.title === 'Brand') {
-                            // api structure is: response.facets
-                            // facets structure for brands is obj.title === brand && obj.properties === Array of brands
-                            return obj.properties;
-                        }
-                    }
-                }
-
-                // If code execution gets here there is an error
-                throw new Error('CategoryCtrl: Did not find a brand object in the facets object from api.');
-
-            }
-
-            /**
-             * @ngdoc method
-             * @name getColorFromFacets
-             * @methodOf methodApp.CategoryCtrl
-             * @params {Object} facet
-             * @returns {Object} color is an object parsed from response.facets when calling the product endpoint
-             *
-             * @description
-             * In order to make use of brand specific data in the category view the CategoryCtrl needs to parse it and
-             * set $scope variables that are accessible in the category.html view. This is a private
-             *
-             */
-            function getColorFromFacets(facets) {
-                var item,
-                    obj;
-
-                for (item in facets) {
-                    if (facets.hasOwnProperty(item)) {
-                        obj = facets[item];
-                        if (obj.title && obj.title === 'Color') {
-                            // api response structure is: response.facets
-                            // facets structure for color is obj.title === brand && obj.properties === Array of brands
-                            console.log('How does app determine the colors? ', obj);
-                            return obj.properties;
-                        }
-                    }
-                }
-                // If code execution gets here there is an error
-                throw new Error('CategoryCtrl: Did not find a brand object in the facets object from api.');
-
-            }
-
-            /**
-             * End private functions for the CategoryCtrl
-             */
-
-            /**
              * 'Public' functions for CategoryCtrl
              */
             $scope.selectBrand = function () {
                 console.log('update for the brand: ', $scope.brand);
             };
 
-//            TODO: Change this to use $routeParams and couple that to the :id or :slug as it may be
-//            console.log('category api info: ', vnApi.getCategory());
+//          TODO: Change this to use $routeParams and couple that to the :id or :slug as it may be
             var slug = {
-                    slug: $location.path().split('/')[2]
-                },
-                categoryRequest = vnApi.Category().get(slug);
+                slug: $location.path().split('/')[2]
+            };
 
-            // TODO: shuffle this so the promises are more intuative and flow logically. Prolly use the $q.all.
-            categoryRequest.$promise.then(function (response) {
+            // TODO: shuffle this so the promises are more intuative and flow logically. Prolly use the $q.all?
+            vnApi.Category().get(slug).$promise.then(function (response) {
                 // Handle the category data
-//                console.log('response: ', response);
                 $scope.category = response.data;
                 // Todo: move the subCategory parsing to product response
-                $scope.subCategories = $scope.category.subCategories;
+//                $scope.subCategories = $scope.category.subCategories;
             }).then(function () {
-                // Handle the products for this category
-                var productRequest = vnApi.Product().query({categoryIds: $scope.category.id});
-                productRequest.$promise.then(function (response) {
-//                    console.log('the response 2nd: ', response);
-//                    console.log('the response: ', response);
+                // Handle the Products & Facets for this page
+                // I hate the nested promise here. it is an anti-pattern
+                vnApi.Product().query({categoryIds: $scope.category.id}).$promise.then(function (response) {
                     $scope.products = response.data;
-                    $scope.facets = getFacetsFromResponse(response);
+                    $scope.facets = response.facets;
+                    $scope.categories = response.categories;
+                    console.log('subCats', $scope.categories);
                 });
             });
         }]);
---
+++ app/scripts/directives/vn-facet-search.js
@@ -0,0 +1,67 @@
+'use strict';
+
+
+/**
+ * @ngdoc directive
+ * @name Volusion.methodApp.directive:vnFacetSearch
+ * @restrict EA
+ * @requires facet object
+ * @scope
+ * @description
+ *
+ * Given a product facet object create the UI for the facets response.
+ * Allow parent controller to be updated with user controlled actions select/de-select
+ *
+ * @usage
+ <div vn-facet-search facets="facets"></div>
+
+ -OR-------------------------------------
+
+ <vn-facet-search facets="facets"></vn-facet-search>
+ *
+ *
+ * @example
+ <example module="Volusion.methodApp" deps="">
+ TODO: Insert example html and javascript.
+ </example>
+ */
+angular.module('methodApp')
+    .directive('vnFacetSearch', function () {
+        return {
+            templateUrl: 'views/vn-facet-search.html',
+            restrict   : 'AE',
+            scope      : {
+                facets    : '=',
+                categories: '='
+            },
+            link       : function postLink(scope) {
+
+                scope.$watch('facets', function (facets) {
+                    scope.facets = facets;
+                });
+
+                scope.$watch('subCategories', function (subCategories) {
+                    scope.subCategories = subCategories;
+                    console.log('scope categories: ', scope.subCategories);
+                });
+
+                scope.selectedFacets = [];
+
+                scope.refineFacetSearch = function (facet) {
+
+                    // Add / Remove facet to selectedFacets
+
+                    if (scope.selectedFacets.indexOf(facet) === -1) {
+                        scope.selectedFacets.push(facet);
+                        console.log('prop not found adding', scope.selectedFacets);
+                    } else {
+                        var index = scope.selectedFacets.indexOf(facet);
+                        scope.selectedFacets.splice(index, 1);
+                        console.log('else assume prop was found and removeing', scope.selectedFacets);
+                    }
+//
+//                    console.log('selectedFacets: ', scope.selectedFacets);
+                };
+            }
+        };
+    });
