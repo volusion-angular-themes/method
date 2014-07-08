fbb5fe5d442c5e14037fbf3f2c9811f6932326c5
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-07-08 15:49:39 -0700

    Comparison re-alignment for page(s), controller(s) code.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -139,3 +139,14 @@ angular.module('Volusion.controllers')
 
 		}
 	]);
+
+$(document).ready(function() {
+	$('[data-toggle="popover"]').popover();
+	$('body').on('click', function (e) {
+		$('[data-toggle="popover"]').each(function () {
+			if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+				$(this).popover('hide');
+			}
+		});
+	});
+});
