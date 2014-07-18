9707e3bdd3de83f1c846828497deb530e14fb009
Matt Hippely <hippely@gmail.com>
2014-07-18 09:28:08 -0700

    Faceted Search: * Remove toolbox templates * update the directive js * Move original _category stylesheet * Create _faceted-search stylesheet * bemized *some* of the facted-search html and scss * Moved action btn's to top

--- app/scripts/directives/faceted-search.js
+++ app/scripts/directives/faceted-search.js
@@ -1,23 +1,23 @@
 /**
- * @ngdoc directive
- * @name methodApp.directive:facetedSearch
- * @restrict EA
- * @requires vnProductParams
- * @scope
- * @description
- * - Used on pages that have a list of products and need to filter or narrow the list by:
- *     - category
- *     - facet
- *     - price
- *     - sort?
- *
- * ## Notes:
- * - The parent controller for the directive muxt implement a queryProducts() function
- * - The Function must implement its get products routine with the vnParamsObject
- *
- * @usage
- * TODO: Add html and javascript here to demo it in docs.
- */
+* @ngdoc directive
+* @name methodApp.directive:facetedSearch
+* @restrict EA
+* @requires vnProductParams
+* @scope
+* @description
+* - Used on pages that have a list of products and need to filter or narrow the list by:
+*     - category
+*     - facet
+*     - price
+*     - sort?
+*
+* ## Notes:
+* - The parent controller for the directive muxt implement a queryProducts() function
+* - The Function must implement its get products routine with the vnParamsObject
+*
+* @usage
+* TODO: Add html and javascript here to demo it in docs.
+*/
 angular.module('methodApp')
 	.directive('facetedSearch', ['vnProductParams', function (vnProductParams) {
 
@@ -30,6 +30,21 @@ angular.module('methodApp')
 
 				scope.showCategorySearch = false;
 				scope.showFacetSearch = false;
+				scope.showApplyButton = false;
+
+				enquire.register('screen and (max-width:767px)', {
+
+					setup: function() {
+						scope.showApplyButton = false;
+					},
+					unmatch: function () {
+						scope.showApplyButton = false;
+					},
+					// transitioning to mobile mode
+					match  : function () {
+						scope.showApplyButton = true;
+					}
+				});
 
 				scope.searchByPrice = function (event) {
 					// Detect the return/enter keypress only
