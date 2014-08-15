c3012c6418f7fef797981ce62f40359ab6e59265
deploy.sh <travis@localhost.localdomain>
2014-08-15 00:03:19 +0000

    publish: #26960 Display Global Errors for Cart Endpoint
    generated from commit 9b571d8262f788c48ecc4d63739f78b1f1a801d9

--- settings/app.js
+++ settings/app.js
@@ -55,7 +55,7 @@ angular.module('app', ['ui.bootstrap','ui.tinymce'])
 				,autoresize_min_height: 50
 				,autoresize_max_height: 1000
 				,skin_url: 'tinymce-skin'
-				,content_css: '/styles/c6451e97.main.css' //and this right here folks is why we can't use textAngular
+				,content_css: '/styles/bcdd55ed.main.css' //and this right here folks is why we can't use textAngular
 			};
 
 			//handle save button
