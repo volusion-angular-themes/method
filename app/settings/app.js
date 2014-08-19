c3f59a93aaf6a7707b27b0f6b055da791804ec77
Kevin Sproles <aspire2x@gmail.com>
2014-08-19 02:08:10 -0500

    polish theme settings * introduce angular-ladda for button loading & success indicator

--- app/settings/app.js
+++ app/settings/app.js
@@ -1,7 +1,7 @@
-angular.module('app', ['ui.bootstrap','ui.tinymce'])
+angular.module('app', ['ui.bootstrap','ui.tinymce','angular-ladda'])
 
-	.controller('ThemeSettingsCtrl', ['$scope', '$http', '$location',
-		function ($scope, $http, $location) {
+	.controller('ThemeSettingsCtrl', ['$scope', '$http', '$location', '$timeout',
+		function ($scope, $http, $location, $timeout) {
 			'use strict';
 
 			/*
@@ -13,18 +13,21 @@ angular.module('app', ['ui.bootstrap','ui.tinymce'])
 
 			//load data on initial page load
 			var apiUrl;
+			var cssUrl;
 			var environment;
 
 			if ($location.absUrl().indexOf('127.0.0.1') >= 0 || $location.absUrl().indexOf('localhost') >= 0) {
 				//in local development environment (i.e. grunt serve)
 				environment = 'dev';
 				apiUrl = '/settings/themeSettings.json';
+				cssUrl =  '/styles/main.css';
 				$scope.debug = true;
 			} else {
 				//in production
 				environment = 'prod';
 				var queryString = $location.search();
 				apiUrl = '/api/v1/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/settings';
+				cssUrl =  '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/main.css';
 			}
 			$http.get(apiUrl)
 				.success(function(data) {
@@ -55,20 +58,25 @@ angular.module('app', ['ui.bootstrap','ui.tinymce'])
 				,autoresize_min_height: 50
 				,autoresize_max_height: 1000
 				,skin_url: 'tinymce-skin'
-				,content_css: '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/main.css' //and this right here folks is why we can't use textAngular
+				,content_css: cssUrl //and this right here folks is why we can't use textAngular
 			};
 
 			//handle save button
 			$scope.save = function () {
 				if (environment === 'dev') {
 					//don't save it since we don't have a way to override the themeSettings.json
+					$scope.isSaving = true;
 					console.log('would be saved if you were in production');
-					window.alert('would be saved if you were in production, but since you\'re not:=, simply copy the debug output to your themeSettings.json file manually.');
+					$timeout(function() {
+						$scope.isSaving = false;
+					},5000)
 				} else {
 
 					//use the Volusion API to overwrite themeSettings.json
+					$scope.isSaving = true;
 					$http.put(apiUrl, $scope.settings)
 						.success(function () {
+							$scope.isSaving = false;
 							console.log('successfully saved');
 						});
 
