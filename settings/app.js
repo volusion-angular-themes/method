9b3e798f5271b73a88d75a90d9bdb840aa40a50f
deploy.sh <travis@localhost.localdomain>
2014-08-27 23:17:12 +0000

    publish: Merge pull request #121 from prabinv/master
    generated from commit 989cd07bc3d169ad91b846f7d07fee0f53bc9f69

--- settings/app.js
+++ settings/app.js
@@ -20,14 +20,14 @@ angular.module('app', ['ui.bootstrap','ui.tinymce','angular-ladda'])
 				//in local development environment (i.e. grunt serve)
 				environment = 'dev';
 				apiUrl = '/settings/themeSettings.json';
-				cssUrl =  '/styles/bb6fdcef.main.css';
+				cssUrl =  '/styles/537211b6.main.css';
 				$scope.debug = true;
 			} else {
 				//in production
 				environment = 'prod';
 				var queryString = $location.search();
 				apiUrl = '/api/v1/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/settings';
-				cssUrl =  '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/bb6fdcef.main.css';
+				cssUrl =  '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/537211b6.main.css';
 			}
 			$http.get(apiUrl)
 				.success(function(data) {
