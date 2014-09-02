91952abf711b70a5490443a98c04f4da8cc29b7a
Prabin Varma <prabinv@gmail.com>
2014-09-02 16:22:49 -0500

    Updated to use vnAppMessage component for cart messages

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -12,28 +12,6 @@ angular.module('Volusion.controllers')
 
 			'use strict';
 
-			$scope.alerts = [];
-
-			$scope.closeAlert = function (id) {
-				$scope.alerts = $filter('filter')($scope.alerts, function (alert) {
-					return alert.id !== id;
-				});
-			};
-
-			$rootScope.$on('vnNotification.show', function (evt, alert) {
-				if (alert.time === undefined) {
-					alert.time = 4000;
-				}
-
-				alert.id = Date.now();
-
-				alert.alertTimer = $timeout(function () {
-					$scope.closeAlert(alert.id);
-				}, alert.time);
-
-				$scope.alerts.push(alert);
-			});
-
 			// Watch the appheader state and update as needed
 			$scope.$watch(
 				function () {
