bc90bc598fbad1c963574b6ab955ce006fdead54
Matt Hippely <hippely@gmail.com>
2014-09-02 09:38:49 -0700

    Debugging travis but not local test failure.

--- test/spec/directives/search-form.js
+++ test/spec/directives/search-form.js
@@ -2,19 +2,20 @@
 
 describe('Directive: searchForm', function () {
 
-  // load the directive's module
-  beforeEach(module('methodApp'));
+	// load the directive's module
+	beforeEach(module('methodApp'));
 
-  var element,
-    scope;
+	var element,
+		scope;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+	beforeEach(inject(function ($rootScope) {
+		scope = $rootScope.$new();
+	}));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<search-form></search-form>');
-    element = $compile(element)(scope);
-    expect(element).toBeDefined();
-  }));
+	it('should make hidden element visible', inject(function ($compile) {
+		element = angular.element('<search-form></search-form>');
+		element = $compile(element)(scope);
+		console.log('Print statement for debugging travis machine issue with build: ', element);
+		expect(element).toBeDefined();
+	}));
 });
