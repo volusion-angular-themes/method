a3a939d9ace2cc35a0cefeedea272fda457dfe89
Matt Hippely <hippely@gmail.com>
2014-09-02 10:42:43 -0700

    No expectations here.

--- test/spec/directives/search-form.js
+++ test/spec/directives/search-form.js
@@ -1,21 +1,21 @@
-//'use strict';
-//
-//describe('Directive: searchForm', function () {
-//
-//	// load the directive's module
-//	beforeEach(module('methodApp'));
-//
-//	var element,
-//		scope;
-//
-//	beforeEach(inject(function ($rootScope) {
-//		scope = $rootScope.$new();
-//	}));
-//
-//	it('should make hidden element visible', inject(function ($compile) {
-//		element = angular.element('<search-form></search-form>');
-//		element = $compile(element)(scope);
-//		console.log('Print statement for debugging travis machine issue with build: ', element);
+'use strict';
+
+describe('Directive: searchForm', function () {
+
+	// load the directive's module
+	beforeEach(module('methodApp'));
+
+	var element,
+		scope;
+
+	beforeEach(inject(function ($rootScope) {
+		scope = $rootScope.$new();
+	}));
+
+	it('should make hidden element visible', inject(function ($compile) {
+		element = angular.element('<search-form></search-form>');
+		element = $compile(element)(scope);
+		console.log('Print statement for debugging travis machine issue with build: ', element);
 //		expect(element).toBeDefined();
-//	}));
-//});
+	}));
+});
