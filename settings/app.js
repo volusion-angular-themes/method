24ce0c4e078993fb018555276b0bd6f30dd83c83
deploy.sh <travis@localhost.localdomain>
2014-09-03 22:10:59 +0000

    publish: Merge f0f64f5a0304d0e68a4cbec2ad9e6943c0ec21d4 into bdecf68d635a0d99c0e7c851a9d42059d8f5eab5
    generated from commit 6587d984e39212f373ce83e223bf68adad5a8d80

--- settings/app.js
+++ settings/app.js
@@ -20,14 +20,14 @@ angular.module('app', ['ui.bootstrap','ui.tinymce','angular-ladda'])
 				//in local development environment (i.e. grunt serve)
 				environment = 'dev';
 				apiUrl = '/settings/themeSettings.json';
-				cssUrl =  '/styles/fd5bcad9.main.css';
+				cssUrl =  '/styles/c8f3a9e4.main.css';
 				$scope.debug = true;
 			} else {
 				//in production
 				environment = 'prod';
 				var queryString = $location.search();
 				apiUrl = '/api/v1/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/settings';
-				cssUrl =  '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/fd5bcad9.main.css';
+				cssUrl =  '/themes/' + queryString.themeName + '/versions/' + queryString.themeVersion + '/styles/c8f3a9e4.main.css';
 			}
 			$http.get(apiUrl)
 				.success(function(data) {
