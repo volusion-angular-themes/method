bcc7b5f833f18bc962c7367a9b5cbb199c4f5a98
Prabin Varma <prabinv@gmail.com>
2014-09-09 12:12:37 -0500

    moved html, reverse and seo-friendly filters

--- app/scripts/filters/reverse.js
+++
@@ -1,9 +0,0 @@
-angular.module('Volusion.filters')
-	.filter('reverse', function() {
-
-		'use strict';
-
-		return function(items) {
-			return (items === undefined) ? null : items.slice().reverse();
-		};
-	});
