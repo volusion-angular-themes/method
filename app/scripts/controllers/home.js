1c2e47462469e2b986500f8970526236e771b1bc
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-09-22 15:39:18 -0700

    #30000 REFACTOR - Home.js

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,18 +4,10 @@ angular.module('Volusion.controllers')
 
 			'use strict';
 
-			var imagesToPreload = [];
-
-			angular.forEach($scope.themeSettings.pages.home.slider.slides, function (slide) {
-				imagesToPreload.push(slide.imageUrl);
-			});
-
 			vnApi.Product().get({ filter: 'featured', pageSize: 4 }).$promise
 				.then(function (response) {
 					$scope.featuredProducts = response.data;
 				});
-
-			vnImagePreloader.preloadImages(imagesToPreload);
 
 			$scope.getImagePath = function (imageCollections) {
 				var path = $filter('vnProductImageFilter')(imageCollections);
