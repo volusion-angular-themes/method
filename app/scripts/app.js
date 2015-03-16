3cf00d87b8d18cc032db0aa4f742500477e4041f
Brad Sobie <brad_sobie@volusion.com>
2015-03-16 11:03:36 -0500

    Moving routes to toolbox

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,8 +29,8 @@ angular.module('methodApp', [
 	'Volusion.templates'
 ])
 
-  .config(['$locationProvider', 'translateProvider', 'vnAppConfigProvider', 'ENV', '$stateProvider', '$urlRouterProvider',
-	  function ($locationProvider, translateProvider, vnAppConfigProvider, ENV, $stateProvider, $urlRouterProvider) {
+  .config(['$locationProvider', 'translateProvider', 'vnAppConfigProvider', 'ENV',
+	  function ($locationProvider, translateProvider, vnAppConfigProvider, ENV) {
 
 		  'use strict';
 
@@ -47,137 +47,6 @@ angular.module('methodApp', [
 		  };
 
 		  translateProvider.configure(translateOptions);
-
-
-		$urlRouterProvider.otherwise('/');
-
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: 'views/home.html',
-				controller : 'HomeCtrl',
-				resolve    : {
-					translations: ['translate', function (translate) {
-						translate.addParts('home');
-						translate.addParts('product');
-					}]
-				}
-			})
-			.state('login', {
-				url: '/login',
-				templateUrl: 'login/login.html',
-				controller : 'LoginCtrl',
-				resolve : {
-					emptyCart : ['vnAppRoute', function (vnAppRoute) {
-						return vnAppRoute.checkEmptyCart(this.url.source);
-					}]
-				}
-			})
-			.state('checkout', {
-				url: '/checkout',
-				templateUrl: 'checkout/checkout.html',
-				controller : 'CheckoutCtrl',
-				resolve : {
-					emptyCart : ['vnAppRoute', function (vnAppRoute) {
-						return vnAppRoute.checkEmptyCart(this.url.source);
-					}]
-				}
-			})
-			.state('thank-you', {
-				url: '/thank-you',
-				templateUrl: 'thank-you/thank-you.html',
-				controller : 'ThankYouCtrl'
-			})
-			.state('product', {
-				url: '/p/:slug',
-				templateUrl: 'views/product.html',
-				controller : 'ProductCtrl',
-				resolve    : {
-					translations: ['translate', function (translate) {
-						return translate.addParts('product');
-					}]
-				}
-			})
-			.state('category', {
-				url: '/c/:slug',
-				templateUrl   : 'views/category.html',
-				controller    : 'CategoryCtrl',
-				resolve       : {
-					params      : ['vnAppRoute', '$location', function (vnAppRoute, $location) {
-						return vnAppRoute.resolveParams($location.search());
-					}],
-					translations: ['translate', function (translate) {
-						translate.addParts('product');
-					}]
-				}
-			})
-			.state('search', {
-				url: '/search',
-				templateUrl   : 'views/search.html',
-				controller    : 'SearchCtrl',
-				reloadOnSearch: false,
-				resolve       : {
-					params      : ['vnAppRoute', '$location', function (vnAppRoute, $location) {
-						return vnAppRoute.resolveParams($location.search());
-					}],
-					translations: ['translate', function (translate) {
-						translate.addParts('product');
-					}]
-				}
-			})
-			.state('allProducts', {
-				url: '/all-products',
-				templateUrl   : 'views/search.html',
-				controller    : 'SearchCtrl',
-				reloadOnSearch: false,
-				resolve       : {
-					params      : ['vnAppRoute', '$location', function (vnAppRoute, $location) {
-						return vnAppRoute.resolveParams($location.search());
-					}],
-					translations: ['translate', function (translate) {
-						translate.addParts('product');
-					}]
-				}
-			})
-			.state('themeSettings', {
-				url: '/theme-settings',
-				templateUrl: 'views/theme-settings.html',
-				controller : 'ThemeSettingsCtrl'
-			})
-			.state('article', { // Articles must be last or the prior /search and /theme-settings will never be picked up
-				url: '/:slug',
-				templateUrl: 'views/article.html',
-				controller : 'PageCtrl',
-                resolve: {
-                    article: ['vnApi', '$route', function (vnApi, $route) {
-                        return vnApi.Article().get({slug: $route.current.params.slug}).$promise;
-                    }]
-                }
-			});
-
-			function getCartState(){
-				return {
-					url: '/cart',
-					onEnter: ['$rootScope', function($rootScope) {
-						$rootScope.$emit('enterCartState');
-				    }],
-				    onExit: ['$rootScope', function($rootScope) {
-				    	$rootScope.$emit('exitCartState');
-				    }]
-				};
-			}
-
-			//Register cart states for all pages
-			$stateProvider
-				.state('home.cart', getCartState())
-				.state('product.cart', getCartState())
-				.state('category.cart', getCartState())
-				.state('search.cart', getCartState())
-				.state('allProducts.cart', getCartState())
-				.state('themeSettings.cart', getCartState())
-				.state('article.cart', getCartState())
-				.state('login.cart', getCartState())
-				.state('thank-you.cart', getCartState());
 
 	}])
 
