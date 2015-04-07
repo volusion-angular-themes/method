872a9187a059738ec67538ddb42ed9170b51db3c
Osei Bonsu <osei_bonsu@volusion.com>
2015-04-07 13:18:48 -0500

    update deploy script

---
+++ deploy.js
@@ -0,0 +1,32 @@
+var sys = require('sys');
+var exec = require('child_process').execSync;
+var version = process.argv[2];
+var toolboxVersion = process.argv[3];
+
+console.log('Version: ' + version);
+console.log('Toolbox Version: ' + toolboxVersion);
+
+var packageJson = require('./package.json');
+var bowerJson = require('./bower.json');
+var themeInfoJson = require('./app/settings/themeInfo.json');
+
+var oldVersion = packageJson.version;
+packageJson.version = version;
+bowerJson.version = version;
+themeInfoJson.version = version;
+themeInfoJson.source = 'https://github.com/volusion-angular-themes/method/archive/v' + version + '.zip';
+
+var fs = require('fs');
+fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+console.log('package.json has been updated');
+fs.writeFileSync('./bower.json', JSON.stringify(bowerJson, null, 2));
+console.log('bower.json has been updated');
+fs.writeFileSync('./app/settings/themeInfo.json', JSON.stringify(themeInfoJson, null, 2));
+console.log('themeInfo.json has been updated');
+
+//
+//exec('git commit -am "Bumping version from ' + oldVersion + ' to ' + version + '"', {cwd: './public'});
+//exec('git tag -a ' + version + ' -m "Version ' + version + '" -f ', {cwd: './public'});
+//console.log('Run the following command in the public directory');
+//console.log('to make the package available in Bower:');
+//console.log("git push origin master --tags");
