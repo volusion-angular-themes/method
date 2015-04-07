b7f836a5c4c588d73f791b918b3d568274a7db08
Osei Bonsu <osei_bonsu@volusion.com>
2015-04-07 16:22:49 -0500

    Add git actions to deploy script

--- deploy.js
+++ deploy.js
@@ -6,6 +6,10 @@ var toolboxVersion = process.argv[3];
 console.log('Version: ' + version);
 console.log('Toolbox Version: ' + toolboxVersion);
 
+
+console.log('Hubflow: Start Release');
+exec('git hf release start ' + version);
+
 var packageJson = require('./package.json');
 var bowerJson = require('./bower.json');
 var themeInfoJson = require('./app/settings/themeInfo.json');
@@ -13,6 +17,7 @@ var themeInfoJson = require('./app/settings/themeInfo.json');
 var oldVersion = packageJson.version;
 packageJson.version = version;
 bowerJson.version = version;
+bowerJson.dependencies['vn-toolbox-public'] = '~' + toolboxVersion;
 themeInfoJson.version = version;
 themeInfoJson.source = 'https://github.com/volusion-angular-themes/method/archive/v' + version + '.zip';
 
@@ -21,10 +26,14 @@ fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
 console.log('package.json has been updated');
 fs.writeFileSync('./bower.json', JSON.stringify(bowerJson, null, 2));
 console.log('bower.json has been updated');
-fs.writeFileSync('./app/settings/themeInfo.json', JSON.stringify(themeInfoJson, null, 2));
+fs.writeFileSync('./app/settings/themeInfo.json', JSON.stringify(themeInfoJson, null, 4));
 console.log('themeInfo.json has been updated');
 
-//
+exec('git commit -am "bumped version from ' + oldVersion + ' to ' + version + '"')
+exec('git hf release finish ' + version);
+exec('git tag -a ' + version + ' -m "Version ' + version + '" -f ');
+console.log('Hubflow: Finish Release');
+
 //exec('git commit -am "Bumping version from ' + oldVersion + ' to ' + version + '"', {cwd: './public'});
 //exec('git tag -a ' + version + ' -m "Version ' + version + '" -f ', {cwd: './public'});
 //console.log('Run the following command in the public directory');
