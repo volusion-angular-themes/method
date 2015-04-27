c9167af7c5400af17ba93d4a211b3aaea372587d
Brad Sobie <brad_sobie@volusion.com>
2015-04-27 12:30:54 -0500

    Shifting code around
    Moving article route into theme because it needs to be the last route
    loaded. This will allow custom routes in the theme

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,8 +29,8 @@ angular.module('methodApp', [
 	'Volusion.templates'
 ])
 
-  .config(['translateProvider', 'vnAppConfigProvider', 'ENV',
-	  function (translateProvider, vnAppConfigProvider, ENV) {
+  .config(['translateProvider', 'vnAppConfigProvider', 'ENV', '$stateProvider',
+	  function (translateProvider, vnAppConfigProvider, ENV, $stateProvider) {
 
 		  'use strict';
 
@@ -46,6 +46,24 @@ angular.module('methodApp', [
 
 		  translateProvider.configure(translateOptions);
 
+		  $stateProvider
+			  /* Sample custom route
+			  .state('about', {
+			   	  parent: 'root',
+				  url: '/about',
+				  template: '<div class="container"><h1>About</h1></div>'
+			  })*/
+			  .state('article', { // Articles must be last or the prior /search and /theme-settings will never be picked up
+				  parent: 'root',
+				  url: '/:slug',
+				  templateUrl: 'views/article.html',
+				  controller: 'PageCtrl',
+				  resolve: {
+					  article: ['vnApi', '$stateParams', function (vnApi, $stateParams) {
+						  return vnApi.Article().get({slug: $stateParams.slug}).$promise;
+					  }]
+				  }
+			  });
 	}])
 
 .run(['snapRemote', '$rootScope', '$window', 'themeSettings', 'vnCart', 'translate', 'vnModalService',
@@ -72,10 +90,6 @@ angular.module('methodApp', [
 				snapRemote.close();
 			}
 
-		});
-
-		$rootScope.$on('$stateChangeSuccess', function (event, toState) {
-			$rootScope.currentState = toState.name;
 		});
 
 		$rootScope.$on('VN_HTTP_500_ERROR', function () {
