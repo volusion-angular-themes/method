a92d2396371b67c4665eddb1bbaed24bbef03e29
Brad Sobie <brad_sobie@volusion.com>
2015-05-05 16:35:45 -0500

    Fixing MainCtrl tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,32 +1,44 @@
 //Disabling until we can get vnDevice to work in this test
 
-/*describe('Controller: MainCtrl', function () {
+describe('Controller: MainCtrl', function () {
 
 	'use strict';
+
+	var MainCtrl,
+		scope,
+		vnDevice,
+		vnSiteConfig;
 
 	// load the controller's module
 	beforeEach(module('methodApp'));
 
-	var MainCtrl,
-		scope,
-		vnDevice;
-
-	beforeEach(inject(function ($controller, $rootScope, _vnDevice_) {
+	beforeEach(inject(function ($controller, $rootScope, _vnSiteConfig_, _vnDevice_) {
 		vnDevice = _vnDevice_;
+		vnSiteConfig = _vnSiteConfig_;
 
-		spyOn(vnDevice, "init").and.returnValue(true);
+		spyOn(vnDevice, "init");
+		spyOn(vnSiteConfig, "getConfig").and.returnValue({
+			then: function(response) {
+				return response;
+			}
+		});
 
 		scope = $rootScope.$new();
 		MainCtrl = $controller('MainCtrl', {
 			$scope: scope
 		});
+
 	}));
 
 	it('should exist', function() {
 		expect(MainCtrl).toBeDefined();
 	});
 
+	it('should initialize vnDevice', function() {
+		expect(vnDevice.init).toHaveBeenCalled();
+	});
+
 	it('should call SiteConfig', function() {
 		expect(vnSiteConfig.getConfig).toHaveBeenCalled();
 	});
-});*/
+});
