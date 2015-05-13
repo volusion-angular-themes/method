7d44e67fcd92ae96492e364ed7fe60c40fdd75a3
Brad Sobie <brad_sobie@volusion.com>
2015-05-12 20:56:12 -0500

    Created new mobile menu/removed snap.js dependency
    Snap remote was causing a lot of layout issues because it was absolute
    positioning the main content area, which is very non-standard. Also
    since we’re not using it anymore for the cart, there’s really no need
    for it.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,7 +14,6 @@ angular.module('methodApp', [
 	// Third party modules
 	'ui.bootstrap',
 	'pascalprecht.translate',
-	'snap',
 	'textAngular',
 	'ui.router',
 	'ngRoute',
@@ -67,8 +66,8 @@ angular.module('methodApp', [
 			  });
 	}])
 
-.run(['snapRemote', '$rootScope', '$window', 'themeSettings', 'vnCart', 'translate', 'vnModalService',
-	function (snapRemote, $rootScope, $window, themeSettings, vnCart, translate, vnModalService) {
+.run(['$rootScope', '$window', 'themeSettings', 'vnCart', 'translate', 'vnModalService',
+	function ($rootScope, $window, themeSettings, vnCart, translate, vnModalService) {
 
 		'use strict';
 
@@ -80,14 +79,18 @@ angular.module('methodApp', [
 
 		$rootScope.$on('$stateChangeStart', function () {
 			$window.scrollTo(0, 0);
-			snapRemote.close();
+
 			if($rootScope.isCartOpen){
 				$rootScope.closeCart();
 			}
 			else{
 				$window.scrollTo(0, 0);
-				snapRemote.close();
 			}
+
+			if($rootScope.isMobileMenuOpen){
+				$rootScope.closeMobileMenu();
+			}
+
 		});
 
 		$rootScope.$on('VN_HTTP_500_ERROR', function () {
